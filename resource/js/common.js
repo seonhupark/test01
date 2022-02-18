ba142ffb05828fe6b368dd111b20103e0d3c9d60
seonhupark <seonhupark@mindslab.ai>
2022-02-18 14:33:14 +0900

    22

--- resource/js/common.js
+++
@@ -1,9 +0,0 @@
-
-const imageSequence = document.querySelector(".image_sequence");
-
-let i=0;
-let imgSeq = setInterval(()=>{
-    if( i<74 ) i++; // 한 번 재생
-    else i=0; // loop play
-    imageSequence.src=`resource/img/${i}.png`;
-},40)
